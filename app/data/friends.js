c9ac0479e22170462ae91aed4d688b0575b2d5e9
Lawrence Fiesta <lfiesta86@gmail.com>
2019-07-09 15:55:08 -0700

    added the home html skeleton and a few changes to the friend array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,19 @@
 var friends = [
   {
-    Name: "L",
-    Photo: "kiki",
-    Score: ["1","2","3","4","5","6","7","8","9","1"]
+    Name: "Lawrence",
+    Photo: "https://avatars2.githubusercontent.com/u/26019640?s=400&u=64687b6c849fa804aa09540ac830d631ab56eb86&v=4",
+    Score: ["4","3","2","4","1","2","2","4","4","4"]
   },
   {
-    Name: "M",
-    Photo: "do",
+    Name: "Keanu Reeves",
+    Photo: "https://cdnph.upi.com/svc/sv/upi/6661555345522/2019/1/5f615a4b5eb2597b9fa26f2e5903e407/Keanu-Reeves-John-Wick-is-fighting-for-his-life-and-soul.jpg",
     Score: ["3","3","3","3","3","3","3","3","3","3"]
   },
   {
-    Name: "N",
-    Photo: "you",
+    Name: "Emma Stone",
+    Photo: "https://m.media-amazon.com/images/M/MV5BMjI4NjM1NDkyN15BMl5BanBnXkFtZTgwODgyNTY1MjE@._V1.._UX214_CR0,0,214,317_AL_.jpg",
     Score: ["5","5","5","5","5","5","5","5","5","5"]
-  },
+  }
 ]
 
 module.exports = friends;
